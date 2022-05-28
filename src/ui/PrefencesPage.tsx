6782ebb55abe6f4dbbdf68d149f46a9387be7535
V <s19159@pjwstk.edu.pl>
2022-05-28 16:34:34 +0200

    added Ftx uri adding and invalid address alerts

--- src/ui/PrefencesPage.tsx
+++ src/ui/PrefencesPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import styled from "styled-components"
-import { Panel, TextPrimary, TextSecondary, COLOR_PANEL, COLOR_ACCENT, COLOR_TEXT_PRIMARY, COLOR_TEXT_SECONDARY } from "./common"
+import { Panel, TextPrimary, TextSecondary, COLOR_PANEL, COLOR_ACCENT, COLOR_TEXT_PRIMARY, COLOR_TEXT_SECONDARY, COLOR_FAILURE } from "./common"
 
 export interface PreferencesProps {
   accounts: string[]
@@ -9,21 +9,36 @@ export interface PreferencesProps {
 
 export const PreferencesPage = ({ accounts, setAccounts }: PreferencesProps) => {
   const [uri, setUri] = useState('');
-  const [accType, setAccType] = useState('');
+  const [secret, setSecret] = useState('');
+  const [accType, setAccType] = useState('Ethereum:');
+  const [problem, setProblem] = useState(false);
 
-  const handleAddAccount = (acc: Array<string>) => {
-    switch (acc[0]) {
+  const handleAddAccount = () => {
+    console.log(accType)
+    switch (accType) {
       case 'Ethereum:':
-        if (!(/0x[a-fA-F0-9]{40}/.test(acc[1]))) {
-          alert("Problem"); 
+        if (!(/0x[a-fA-F0-9]{40}/.test(uri))) {
+          setProblem(true);
           break;
+          //TODO
         }
-        setAccounts([...accounts, acc[0] + acc[1]]);
+        setAccounts([...accounts, accType + uri]);
         setAccType('');
         setUri('');
+        setProblem(false);
         break;
-      case "Ftx":
-      //TODO
+      case "Ftx:":
+        if (!uri || !secret) {
+          setProblem(true);
+          break;
+          //TODO
+        }
+        setAccounts([...accounts, accType + uri+ "?secret=" + secret]);
+        setAccType('');
+        setUri('');
+        setSecret('');
+        setProblem(false);
+        break;
 
     }
 
@@ -39,7 +54,8 @@ export const PreferencesPage = ({ accounts, setAccounts }: PreferencesProps) => 
         <AccountEntry style={index === 0 ? { borderWidth: '1px 0px 1px 0px' } : {}} key={index}>
           {account.split(':')[0]}
           <Button style={{ justifySelf: 'end' }} onClick={() => setAccounts(accounts.filter((a, i) => i !== index))}>Remove</Button>
-          <TextSecondary>Address:</TextSecondary> {account.split(':')[1]}
+          <TextSecondary>Address:</TextSecondary> {account.split(':')[1].split('?')[0]}
+          {account.split(':')[0] === "Ftx" && <><TextSecondary>Secret:</TextSecondary> {account.split(':')[1].split('=')[1]}</>}
         </AccountEntry>
       ))}
 
@@ -48,15 +64,16 @@ export const PreferencesPage = ({ accounts, setAccounts }: PreferencesProps) => 
           <Option value='Ethereum:' >Ethereum</Option>
           <Option value='Ftx:'>Ftx</Option>
         </Select>
-        <Button style={{ justifySelf: 'end' }} onClick={() => handleAddAccount([accType, uri])}>Add</Button>
+        <Button style={{ justifySelf: 'end' }} onClick={() => handleAddAccount()}>Add</Button>
         <TextSecondary>{accType !== 'Ftx:' ? 'Address:' : 'API Key:'}</TextSecondary>
-        <Input value={uri} onChange={(e) => setUri(e.target.value)} />
+        <Input value={uri} style={{borderColor: problem ? COLOR_FAILURE : COLOR_TEXT_SECONDARY }} onChange={(e) => setUri(e.target.value)} />
         {accType === 'Ftx:' &&
           <>
             <TextSecondary style={{ marginTop: '1vh' }}>Secret:</TextSecondary>
-            <Input style={{ marginTop: '1vh' }} value={uri} onChange={(e) => setUri(e.target.value)} />
+            <Input style={{ marginTop: '1vh', borderColor: problem ? COLOR_FAILURE : COLOR_TEXT_SECONDARY }} value={secret} onChange={(e) => setSecret(e.target.value)} />
           </>
         }
+        {problem && <Alert>Invalid address</Alert>}
       </AccountEntry>
     </Panel>
   )
@@ -76,7 +93,7 @@ const AccountEntry = styled.div`
 
 const Input = styled.input`
   width: 400px;
-  font-size: 1.5vh;
+  font-size: 18px;
   color: ${COLOR_TEXT_PRIMARY};
   background-color: ${COLOR_PANEL};
   border-width: 0px 0px 1px 0px;
@@ -84,8 +101,8 @@ const Input = styled.input`
 `
 
 const Select = styled.select`
-  width: 100px;
-  font-size: 1.5vh;
+  width: 120px;
+  font-size: 18px;
   padding: 0.5vh 1vh 0.5vh 0vh ;
   color: white;
   background-color: transparent;
@@ -100,11 +117,16 @@ const Option = styled.option`
 `
 
 const Button = styled.button`
-  font-size: 1.5vh;
+  font-size: 18px;
   padding: 0.5vh 1vh 0.5vh 1vh ;
   color: ${COLOR_ACCENT};
   background-color: transparent;
   border-color: transparent;
   margin-left: 1vh;
   cursor: pointer;
+`
+
+const Alert = styled.p`
+  justify-self: center;
+  color: ${COLOR_FAILURE};
 `
